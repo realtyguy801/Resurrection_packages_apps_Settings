1bddca21adf807bcdab92b8b1d74de8a1841c9a5
Shaun <shaunsibu@gmail.com>
2016-12-15 22:55:43 -0700

    Use correct import
    Change-Id: Ie0d544a311545875f3411b8906f7480e99ee143b

--- src/com/android/settings/rr/input/PowerMenuActions.java
+++ src/com/android/settings/rr/input/PowerMenuActions.java
@@ -25,9 +25,9 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v7.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.internal.util.cm.PowerMenuConstants;
 
