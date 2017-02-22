354a6d11370431b0c0fbc35f927248d4f2eb47c7
ezio84 <brabus84@gmail.com>
2017-02-22 10:01:43 -0700

    Pulse: More bars for solid line renderer [3/3]
    Change-Id: Ifd2feb6faa5714a313d9cd792c17d11046b7b2e9

--- src/com/android/settings/rr/navbar/PulseSettings.java
+++ src/com/android/settings/rr/navbar/PulseSettings.java
@@ -62,6 +62,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     private static final String SOLID_FUDGE = "pulse_solid_fudge_factor";
     private static final String SOLID_LAVAMP_SPEED = "lavamp_solid_speed";
     private static final String FADING_LAVAMP_SPEED = "fling_pulse_lavalamp_speed";
+    private static final String PULSE_SOLID_UNITS_COUNT = "pulse_solid_units_count";
 
     static final int DEFAULT = 0xffffffff;
     static final int DEFAULT_TO = 0xff8080ff;
@@ -83,6 +84,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     SeekBarPreference mSolidFudge;
     SeekBarPreference mSolidSpeed;
     SeekBarPreference mFadingSpeed;
+    SeekBarPreference mSolidCount;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -179,6 +181,13 @@ public class PulseSettings extends SettingsPreferenceFragment implements
                 (SeekBarPreference) findPreference(FADING_LAVAMP_SPEED);
         mFadingSpeed.setValue(fspeed);
         mFadingSpeed.setOnPreferenceChangeListener(this);
+
+        int count = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_SOLID_UNITS_COUNT, 64, UserHandle.USER_CURRENT);
+        mSolidCount =
+                (SeekBarPreference) findPreference(PULSE_SOLID_UNITS_COUNT);
+        mSolidCount.setValue(count);
+        mSolidCount.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -258,6 +267,11 @@ public class PulseSettings extends SettingsPreferenceFragment implements
             int val = (Integer) newValue;
             Settings.Secure.putIntForUser(getContentResolver(),
                     Settings.Secure.FLING_PULSE_LAVALAMP_SPEED, val, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mSolidCount) {
+            int val = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_SOLID_UNITS_COUNT, val, UserHandle.USER_CURRENT);
             return true;
         }
         return false;
