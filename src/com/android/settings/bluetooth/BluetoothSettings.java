dda5bc8778cadbfde9fb4c73eff8fdf35df8b85a
Shaun <shaunsibu@gmail.com>
2017-02-24 11:03:43 -0700

    Missed this one
    Change-Id: I85357fb73a7dc5c42a8327d2b489c68a8e3e165b

--- src/com/android/settings/bluetooth/BluetoothSettings.java
+++ src/com/android/settings/bluetooth/BluetoothSettings.java
@@ -296,7 +296,7 @@ public final class BluetoothSettings extends DeviceListPreferenceFragment implem
         int textId = isDiscovering ? R.string.bluetooth_searching_for_devices :
             R.string.bluetooth_search_for_devices;
 
-        boolean isAcceptAllFilesEnabled = Settings.System.getInt(getContentResolver(),
+        boolean isAcceptAllFilesEnabled = CMSettings.System.getInt(getContentResolver(),
                 CMSettings.System.BLUETOOTH_ACCEPT_ALL_FILES, 0) == 1;
 
         menu.add(Menu.NONE, MENU_ID_SCAN, 0, textId)
